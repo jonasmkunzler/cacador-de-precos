119dcc94798d7b8d47be89eb067112f4ad61bd37
JonasUbuntu <jonasmkunzler@gmail.com>
2024-01-30 10:46:50 -0300

    ajustes na tabelação

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,114 +1,132 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server'
 
-import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
-import { connectToDB } from "@/lib/mongoose";
-import Product from "@/lib/models/product.model";
-import { scrapeAmazonProduct, scrapeKabumProduct, scrapePichauProduct, scrapeTeraByteProduct } from "@/lib/scraper";
-import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import {
+  getLowestPrice,
+  getHighestPrice,
+  getAveragePrice,
+  getEmailNotifType
+} from '@/lib/utils'
+import { connectToDB } from '@/lib/mongoose'
+import Product from '@/lib/models/product.model'
+import {
+  scrapeAmazonProduct,
+  scrapeKabumProduct,
+  scrapePichauProduct,
+  scrapeTeraByteProduct
+} from '@/lib/scraper'
+import { generateEmailBody, sendEmail } from '@/lib/nodemailer'
 
-export const maxDuration = 10; // For the hobby plan Vercel only allows 1 to 10 seconds.
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const maxDuration = 500 // For the hobby plan Vercel only allows 1 to 10 seconds.
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export async function GET(request: Request) {
   try {
-    connectToDB();
+    connectToDB()
 
-    const products = await Product.find({});
+    const products = await Product.find({})
 
-    if (!products) throw new Error("No product fetched");
+    if (!products) throw new Error('No product fetched')
 
     // ======================== 1 SCRAPE LATEST PRODUCT DETAILS & UPDATE DB
     const updatedProducts = await Promise.all(
       products.map(async (currentProduct) => {
         // Scrape product
         let scrapedProduct
-        switch(currentProduct.category) {
+        switch (currentProduct.category) {
           case 'Kabum':
-            scrapedProduct = await scrapeKabumProduct(currentProduct.url);
-            break;
+            scrapedProduct = await scrapeKabumProduct(currentProduct.url)
+            break
           case 'Amazon':
-            scrapedProduct = await scrapeAmazonProduct(currentProduct.url);
-            break;
+            scrapedProduct = await scrapeAmazonProduct(currentProduct.url)
+            break
           case 'Terabyte':
-            scrapedProduct = await scrapeTeraByteProduct(currentProduct.url);
-            break;
+            scrapedProduct = await scrapeTeraByteProduct(currentProduct.url)
+            break
           case 'Pichau':
-            scrapedProduct = await scrapePichauProduct(currentProduct.url);
-            break;
+            scrapedProduct = await scrapePichauProduct(currentProduct.url)
+            break
           case 'Unknown':
-            throw new Error('Invalid source');
+            throw new Error('Invalid source')
         }
 
-        if (!scrapedProduct) return;
+        if (!scrapedProduct) return
 
-        
-        const updatedPriceHistory:any = [
+        const updatedPriceHistory: any = [
           ...currentProduct.priceHistory,
           {
-            price: scrapedProduct.currentPrice,
-          },
-        ];
-        
+            price: scrapedProduct.currentPrice
+          }
+        ]
+
         const product = {
           ...scrapedProduct,
           priceHistory: updatedPriceHistory,
           lowestPrice: getLowestPrice(updatedPriceHistory),
           highestPrice: getHighestPrice(updatedPriceHistory),
-          averagePrice: getAveragePrice(updatedPriceHistory),
-        };
+          averagePrice: getAveragePrice(updatedPriceHistory)
+        }
 
         const updatedProduct = await Product.findOneAndUpdate(
           {
-            url: product.url,
+            url: product.url
           },
           {
             $set: {
-              lowestPrice: getLowestPrice([...currentProduct.priceHistory, { price: scrapedProduct.currentPrice }]),
-              highestPrice: getHighestPrice([...currentProduct.priceHistory, { price: scrapedProduct.currentPrice }]),
-              averagePrice: getAveragePrice([...currentProduct.priceHistory, { price: scrapedProduct.currentPrice }]),
-              isOutOfStock: scrapedProduct.isOutOfStock,
+              lowestPrice: getLowestPrice([
+                ...currentProduct.priceHistory,
+                { price: scrapedProduct.currentPrice }
+              ]),
+              highestPrice: getHighestPrice([
+                ...currentProduct.priceHistory,
+                { price: scrapedProduct.currentPrice }
+              ]),
+              averagePrice: getAveragePrice([
+                ...currentProduct.priceHistory,
+                { price: scrapedProduct.currentPrice }
+              ]),
+              isOutOfStock: scrapedProduct.isOutOfStock
             },
             $push: {
               priceHistory: {
-                price: scrapedProduct.currentPrice,
-              },
+                price: scrapedProduct.currentPrice
+              }
             },
             $inc: {
-              reviewsCount: 1, // Incrementa o reviewsCount mais 1
-            },          
+              reviewsCount: 1 // Incrementa o reviewsCount mais 1
+            }
           },
           { new: true } // Retorna o novo documento após a atualização
-        );
+        )
 
         // ======================== 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
-        const emailNotifType = getEmailNotifType(
-          scrapedProduct,
-          currentProduct
-        );
+        const emailNotifType = getEmailNotifType(scrapedProduct, currentProduct)
 
         if (emailNotifType && updatedProduct.users.length > 0) {
           const productInfo = {
             title: updatedProduct.title,
-            url: updatedProduct.url,
-          };
+            url: updatedProduct.url
+          }
           // Construct emailContent
-          const emailContent = await generateEmailBody(productInfo, emailNotifType);
+          const emailContent = await generateEmailBody(
+            productInfo,
+            emailNotifType
+          )
           // Get array of user emails
-          const userEmails = updatedProduct.users.map((user: any) => user.email);
+          const userEmails = updatedProduct.users.map((user: any) => user.email)
           // Send email notification
-          await sendEmail(emailContent, userEmails);
+          await sendEmail(emailContent, userEmails)
         }
 
-        return updatedProduct;
+        return updatedProduct
       })
-    );
+    )
 
     return NextResponse.json({
-      message: "Ok",
-      data: updatedProducts,
-    });
+      message: 'Ok',
+      data: updatedProducts
+    })
   } catch (error: any) {
-    throw new Error(`Failed to get all products: ${error.message}`);
+    throw new Error(`Failed to get all products: ${error.message}`)
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,25 @@
-import HeroCarousel from "@/components/HeroCarousel"
-import Searchbar from "@/components/Searchbar"
-import Image from "next/image"
-import { getAllProducts } from "@/lib/actions"
-import ProductCard from "@/components/ProductCard"
+import Searchbar from '@/components/Searchbar'
+import { getAllProducts } from '@/lib/actions'
+import ProductCard from '@/components/ProductCard'
 
 const Home = async () => {
-  const allProducts = await getAllProducts();
+  const allProducts = await getAllProducts()
 
   return (
     <>
       <section className="px-6 md:px-20 py-12">
         <div className="justify-center items-center bg-gradient-to-r from-blue-500 to-blue-800 p-8 rounded-xl border-2 text-white">
-          
-            <h1 className="head-text">
-             Acompanhe a variação de preços dos seus produtos:
-              
-            </h1>
+          <h1 className="head-text">
+            Acompanhe a variação de preços dos seus produtos:
+          </h1>
 
-            <p className="mt-6">
-              Adicionando a URL de um produto dos sites KABUM, PICHAU, TERABYTE e AMAZON você passa a receber alertas das variações dos preços deles.
-            </p>
+          <p className="mt-6">
+            Adicionando a URL de um produto dos sites KABUM, PICHAU, TERABYTE e
+            AMAZON você passa a receber alertas das variações dos preços deles.
+          </p>
 
-            <Searchbar />
+          <Searchbar />
         </div>
-
-       
       </section>
 
       <section className="trending-section">
